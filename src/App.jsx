25ce3c6a90e62968f18d3b1ea20f13c09d23483d
priti <pritiparulkar@gmail.com>
2025-06-27 15:48:20 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,30 @@ import AddmissionPage from './pages/AdmissionPage';
 import ContactPage from './pages/ContactPage';
 import Home from './pages/Home';
 import ChatbotComponent from './components/chatbot/chatbotComponents';
-
-
-
+import { useState } from 'react';
 
 const App = () => {
+  const [showPopup, setShowPopup] = useState(true);
 
-  return (
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+
+
+  return
+  (
+    <>
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Saniya Mahesh Patil"
+          studentPhotoUrl="/images/saniya.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+      />
+      </div>
+ 
     <div>
       <Router>
         <Routes>
@@ -26,7 +43,9 @@ const App = () => {
       <ChatbotComponent/>
       </Router>
     </div>
+    </>
   )
 }
+
 
 export default App;
