4fe1866446af7d90b3548e4385c133300d7d0c3a
priti <pritiparulkar@gmail.com>
2025-06-27 15:53:53 +0530

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './styles/Pages.css'
-import { Route, BrowserRouter as Router , Routes } from "react-router-dom";
+import './styles/Pages.css';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AboutPage from './pages/AboutPage';
 import CoursePage from './pages/CoursePage';
 import AddmissionPage from './pages/AdmissionPage';
 import ContactPage from './pages/ContactPage';
 import Home from './pages/Home';
 import ChatbotComponent from './components/chatbot/chatbotComponents';
+import DeveloperInfoPopup from './components/DeveloperInfoPopup';  // Import the popup component
 import { useState } from 'react';
 
 const App = () => {
@@ -15,37 +16,32 @@ const App = () => {
     setShowPopup(false);
   };
 
-
-  return
-  (
-    <>
+  return (
     <div>
-        {/* Your main application content */}
-        <DeveloperInfoPopup
-          show={showPopup}
-          onClose={handleClosePopup}
-          studentName="Saniya Mahesh Patil"
-          studentPhotoUrl="/images/saniya.jpg" // Path to their photo
-          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+      {/* Developer Info Popup */}
+      <DeveloperInfoPopup
+        show={showPopup}
+        onClose={handleClosePopup}
+        studentName="Saniya Mahesh Patil"
+        studentPhotoUrl="/images/saniya.jpg" // Path to their photo
+        uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
       />
-      </div>
- 
-    <div>
+      
+      {/* Main Application */}
       <Router>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<AboutPage/>}/>
-          <Route path="/course" element={<CoursePage/>}/>
-          <Route path="/contact" element={<ContactPage/>}/>
-          <Route path="/admission" element={<AddmissionPage/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/course" element={<CoursePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/admission" element={<AddmissionPage />} />
         </Routes>
-      <ChatbotComponent/>
+
+        {/* Chatbot Component (Should appear across all routes) */}
+        <ChatbotComponent />
       </Router>
     </div>
-    </>
-  )
-}
-
+  );
+};
 
 export default App;
